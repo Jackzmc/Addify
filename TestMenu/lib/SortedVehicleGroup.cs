ac9ea495b869b05f69be74e0a4d57a293833ed97
Jackz <me@jackz.me>
2019-08-07 23:05:10 -0500

    added sorted vehicles libs

---
+++ TestMenu/lib/SortedVehicleGroup.cs
@@ -0,0 +1,120 @@
+﻿using GTA.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addify.lib
+{
+    public class SortedVehicleGroup
+    {
+        public string Name { get;  private set; }
+        public SortedDictionary<string, VehicleHash> Vehicles { get; private set; }
+
+        public SortedVehicleGroup(String name, SortedDictionary<string,VehicleHash> hashes)
+        {
+            this.Name = name;
+            this.Vehicles = hashes;
+        }
+        public SortedVehicleGroup(String name, VehicleHash[] hashes)
+        {
+            this.Name = name;
+            for(int i=0;i<hashes.Length;i++)
+            {
+                var _name = Enum.GetName(typeof(VehicleHash), hashes[i]).ToString();
+                Vehicles.Add(_name, hashes[i]);
+            }
+        }
+
+        #region pub_methods
+        public enum VehicleGroup
+        {
+            Boats,
+            Commerical,
+            Compacts,
+            Coupes,
+            Cycles,
+            Emergency,
+            Helicopters,
+            Industrial,
+            Military,
+            Motorcycles,
+            Muscle,
+            Offroad,
+            Planes,
+            Sedans,
+            Service,
+            Sports,
+            SportsClassic,
+            Super,
+            SUVs,
+            Utility,
+            Vans,
+            DLC_ImportExport
+        }
+        public static SortedVehicleGroup[] GetVehicleGroups()
+        {
+            VehicleGroup[] groups = (VehicleGroup[])Enum.GetValues(typeof(VehicleGroup));
+            var list = new List<SortedVehicleGroup>();
+            for (int i = 0; i < groups.Length; i++)
+            {
+                var group = GetGroup(groups[i]);
+                if (group != null) list.Add(group);
+            }
+            return list.ToArray();
+        }
+        public static SortedVehicleGroup GetGroup(VehicleGroup group)
+        {
+            switch (group)
+            {
+                case VehicleGroup.Boats:
+                    return new SortedVehicleGroup("Boats", SortedVehicles.BOATS);
+                case VehicleGroup.Commerical:
+                    return new SortedVehicleGroup("Commerical", SortedVehicles.COMMERICAL);
+                case VehicleGroup.Coupes:
+                    return new SortedVehicleGroup("Coupes", SortedVehicles.COUPES);
+                case VehicleGroup.Cycles:
+                    return new SortedVehicleGroup("Cycles", SortedVehicles.CYCLES);
+                case VehicleGroup.Emergency:
+                    return new SortedVehicleGroup("Emergency", SortedVehicles.EMERGENCY);
+                case VehicleGroup.Helicopters:
+                    return new SortedVehicleGroup("Helicopters", SortedVehicles.HELICOPTERS);
+                case VehicleGroup.Industrial:
+                    return new SortedVehicleGroup("Industrial", SortedVehicles.INDUSTRIAL);
+                case VehicleGroup.Military:
+                    return new SortedVehicleGroup("Military", SortedVehicles.MILITARY);
+                case VehicleGroup.Motorcycles:
+                    return new SortedVehicleGroup("Motorcycles", SortedVehicles.MOTORCYCLES);
+                case VehicleGroup.Offroad:
+                    return new SortedVehicleGroup("Offroad", SortedVehicles.OFFROAD);
+                case VehicleGroup.Planes:
+                    return new SortedVehicleGroup("Planes", SortedVehicles.PLANES);
+                case VehicleGroup.Sedans:
+                    return new SortedVehicleGroup("Sedans", SortedVehicles.SEDANS);
+                case VehicleGroup.Service:
+                    return new SortedVehicleGroup("Service", SortedVehicles.SERVICE);
+                case VehicleGroup.Sports:
+                    return new SortedVehicleGroup("Sports", SortedVehicles.SPORTS);
+                case VehicleGroup.SportsClassic:
+                    return new SortedVehicleGroup("Sports Classic", SortedVehicles.SPORTS_CLASSIC);
+                case VehicleGroup.Super:
+                    return new SortedVehicleGroup("Super", SortedVehicles.SUPER);
+                case VehicleGroup.SUVs:
+                    return new SortedVehicleGroup("SUVs", SortedVehicles.SUV);
+                case VehicleGroup.Utility:
+                    return new SortedVehicleGroup("Utility", SortedVehicles.UTILITY);
+                case VehicleGroup.Vans:
+                    return new SortedVehicleGroup("Vans", SortedVehicles.VANS);
+                default:
+                    return null;
+            }
+        }
+        #endregion
+/*
+ * case VehicleGroup.Boats:
+            return new SortedVehicleGroup("", SortedVehicles.);
+            */
+
+}
+}
--- TestMenu/lib/SortedVehicles.cs
+++ TestMenu/lib/SortedVehicles.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GTA.Native;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,116 @@ namespace Addify.lib
 {
     class SortedVehicles
     {
+        public static readonly VehicleHash[] UNSORTED_VEHICLES = new VehicleHash[]
+        {
+            VehicleHash.Adder, VehicleHash.Airbus, VehicleHash.Airtug, VehicleHash.Akuma, VehicleHash.Asea, VehicleHash.Asea2, VehicleHash.Asterope, VehicleHash.Bagger, VehicleHash.BaleTrailer, VehicleHash.Baller, VehicleHash.Baller2, VehicleHash.Banshee, VehicleHash.Barracks, VehicleHash.Bati, VehicleHash.Bati2, VehicleHash.Benson, VehicleHash.BfInjection, VehicleHash.Biff, VehicleHash.Bison, VehicleHash.Bison2, VehicleHash.Bison3, VehicleHash.BJXL, VehicleHash.Blazer, VehicleHash.Blazer2, VehicleHash.Blazer3, VehicleHash.Blimp, VehicleHash.Blista, VehicleHash.BoatTrailer, VehicleHash.BobcatXL, VehicleHash.Boxville, VehicleHash.Boxville2, VehicleHash.Boxville3, VehicleHash.Buccaneer, VehicleHash.Bullet, VehicleHash.Carbonizzare, VehicleHash.Cavalcade, VehicleHash.Cavalcade2, VehicleHash.Cheetah, VehicleHash.Coach, VehicleHash.CogCabrio, VehicleHash.Comet2, VehicleHash.Coquette, VehicleHash.Cruiser, VehicleHash.Crusader, VehicleHash.Cuban800, VehicleHash.Daemon, VehicleHash.Dilettante, VehicleHash.Dilettante2, VehicleHash.DLoader, VehicleHash.DockTrailer, VehicleHash.Docktug, VehicleHash.Dominator, VehicleHash.Double, VehicleHash.Dubsta, VehicleHash.Dubsta2, VehicleHash.Dune, VehicleHash.Dune2, VehicleHash.Elegy2, VehicleHash.Emperor, VehicleHash.Emperor2, VehicleHash.Emperor3, VehicleHash.EntityXF, VehicleHash.Exemplar, VehicleHash.F620, VehicleHash.Faggio2,
+        };
+        public static readonly SortedDictionary<string, VehicleHash> HELICOPTERS = new SortedDictionary<string, VehicleHash>()
+        {
+            {"Annihilator", VehicleHash.Annihilator },
+            {"Buzzard", VehicleHash.Buzzard },
+            {"Buzzard 2", VehicleHash.Buzzard2 },
+            {"Cargbob", VehicleHash.Cargobob },
+            {"Cargobob 2", VehicleHash.Cargobob2 },
+            {"Cargobob3 ", VehicleHash.Cargobob3 },
+            {"Cargobob4", VehicleHash.Cargobob4 },
+            {"Frogger", VehicleHash.Frogger },
+            {"Frogger FBI",VehicleHash.Frogger2 },
+            {"Police Heli",VehicleHash.Polmav }
+        };
+        /*public readonly VehicleHash[] HELICOPTERS = new VehicleHash[]
+        {
+            VehicleHash.Annihilator, VehicleHash.Buzzard, VehicleHash.Buzzard2, VehicleHash.Cargobob, VehicleHash.Cargobob2, VehicleHash.Cargobob3, VehicleHash.Frogger, VehicleHash.Frogger2, VehicleHash.Polmav
+        };*/
+        public static readonly VehicleHash[] EMERGENCY = new VehicleHash[]
+        {
+            VehicleHash.Ambulance, VehicleHash.FireTruck, VehicleHash.FBI, VehicleHash.FBI2, VehicleHash.Police, VehicleHash.Police2, VehicleHash.Police3, VehicleHash.Police4, VehicleHash.Policeb, VehicleHash.PoliceOld1, VehicleHash.PoliceOld2, VehicleHash.PoliceT
+        };
+        public static readonly VehicleHash[] MILITARY = new VehicleHash[]
+        {
+            VehicleHash.ArmyTanker, VehicleHash.ArmyTrailer, VehicleHash.ArmyTrailer2, VehicleHash.Barracks, VehicleHash.Barracks2, VehicleHash.Barracks3
+        };
+        public static readonly VehicleHash[] CYCLES = new VehicleHash[]
+        {
+            VehicleHash.Bmx
+        };
+        public static readonly VehicleHash[] BOATS = new VehicleHash[]
+        {
+            VehicleHash.Dinghy, VehicleHash.Dinghy2, VehicleHash.Dinghy3, VehicleHash.Dinghy4
+        };
+        public static readonly VehicleHash[] COMMERICAL = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] COMPACTS = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] COUPES = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] INDUSTRIAL = new VehicleHash[]
+        {
+            VehicleHash.Bulldozer, VehicleHash.Cutter, VehicleHash.Dump
+        };
+        public static readonly VehicleHash[] MOTORCYCLES = new VehicleHash[]
+        {
+            VehicleHash.Buffalo, VehicleHash.Buffalo2, VehicleHash.Buffalo3
+        };
+        public static readonly VehicleHash[] MUSCLE = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] OFFROAD = new VehicleHash[]
+        {
+            VehicleHash.Bodhi2
+        };
+        public static readonly VehicleHash[] PLANES = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] SEDANS = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] SERVICE = new VehicleHash[]
+        {
+            VehicleHash.Bus, VehicleHash.Caddy, VehicleHash.Caddy2, VehicleHash.TowTruck, VehicleHash.TowTruck2
+        };
+        public static readonly VehicleHash[] SPORTS = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] SPORTS_CLASSIC = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] SUPER = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] SUV = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] UTILITY = new VehicleHash[]
+        {
+
+        };
+        public static readonly VehicleHash[] VANS = new VehicleHash[]
+        {
+            VehicleHash.Burrito, VehicleHash.Burrito2, VehicleHash.Burrito3, VehicleHash.Burrito4, VehicleHash.Burrito5, VehicleHash.Camper
+        };
+        public static readonly VehicleHash[] IMPORT_EXPORT = new VehicleHash[]
+        {
+            VehicleHash.Elegy,  VehicleHash.Tempesta,  VehicleHash.ItaliGTB,  VehicleHash.ItaliGTB2,  VehicleHash.Nero,  VehicleHash.Nero2,  VehicleHash.Specter,  VehicleHash.Specter2,  VehicleHash.Diablous2,  VehicleHash.Blazer5,  VehicleHash.Ruiner2,  VehicleHash.Dune4,  VehicleHash.Dune5,  VehicleHash.Phantom2,  VehicleHash.Voltic2,  VehicleHash.Penetrator,  VehicleHash.Boxville5,  VehicleHash.Wastelander,  VehicleHash.Technical2,  VehicleHash.FCR,  VehicleHash.FCR2,  VehicleHash.Comet3,  VehicleHash.ruiner3
+        };
+        public static readonly VehicleHash[] GUNRUNNING = new VehicleHash[]
+        {
+
+        };
+        
     }
+    
 }
